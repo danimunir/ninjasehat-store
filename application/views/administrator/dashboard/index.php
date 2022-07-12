c4090fc44b5bc69a9acb882bafdd7c84c0b5ad61
Dhani Munir Supriyadi <danimmunir1@gmail.com>
2022-07-12 18:13:12 +0700

    application

---
+++ application/views/administrator/dashboard/index.php
@@ -0,0 +1,24 @@
+        <div class="row">
+			<ol class="breadcrumb">
+				<li><a href="<?=base_url('/administrator.html')?>"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+				<li class="active">Dashboard</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">
+                    <i class="fa fa-dashboard"></i>
+                    Selamat Datang <?=$this->session->userdata('administrator')['nama']?>
+					
+                </h1>
+			</div>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-md-12">
+				<p>Aplikasi Penjualan Obat ini merupakan Tugas UAS <br />
+					Tugas ini dikerjakan oleh Kelompok 3. <br />
+					UNIVERSITAS Bina Saran Informatika</p>
+			</div>
+		</div><!--/.row-->
